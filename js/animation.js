519869d7fc2df9f583137db93cd60ace027f3105
Ivan Blažević <35304314+blazevici@users.noreply.github.com>
2018-07-27 17:50:01 +0200

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,74 +1,49 @@
 $("document").ready(function() {
     
+    var con1 = $(".container1");
+    var con2 = $(".container2");
+
     $(".left-arrow").hover(function() {
-        $(this).attr("src", "Assets/arrow-blue-left.png");
+        $("#left").attr("src", "Assets/arrow-blue-left.png");
     }, function() {
-        $(this).attr("src", "Assets/arrow-gray-left.png");
+        $("#left").attr("src", "Assets/arrow-gray-left.png");
     });
     
     $(".right-arrow").hover(function() {
-        $(this).attr("src", "Assets/arrow-blue-right.png");
+        $("#right").attr("src", "Assets/arrow-blue-right.png");
     }, function() {
-        $(this).attr("src", "Assets/arrow-gray-right.png");
+        $("#right").attr("src", "Assets/arrow-gray-right.png");
     });
-    
-    $(".right-arrow").click(function() {
-        var current = $(".active");
-        var next = current.next();
-        var first = $(".images img:first-child");
-        console.log("..dsa");
-        
-        if(next.length) {
-            next.css("opacity", "0");
-            current.animate({
-                opacity: 0
-            }, 300);
-            current.removeClass("active");
-            next.animate({
-                opacity: 1
-            }, 300);
-            next.addClass("active");
-        }
-        else {
-            first.css("opacity", "0.1");
-            current.animate({
-                opacity: 0
-            }, 300);
-            current.removeClass("active");
-            first.animate({
-                opacity: 1
-            }, 300);
-            first.addClass("active");
-        }
-    });
-    
+
     $(".left-arrow").click(function() {
-        var current = $(".active");
-        var previous = current.prev();
-        var last = $(".images img:last-child");
+        con1.css("opacity", "0");
+        con2.css("opacity", "0");
         
-        if(previous.length) {
-            previous.css("opacity", "0");
-            current.animate({
-                opacity: 0
-            }, 300);
-            current.removeClass("active");
-            previous.animate({
-                opacity: 1
-            }, 300);
-            previous.addClass("active");
-        }
-        else {
-            last.css("opacity", "0");
-            current.animate({
-                opacity: 0
-            }, 300);
-            current.removeClass("active");
-            last.animate({
-                opacity: 1
-            }, 300);
-            last.addClass("active");
-        }
+        con1.animate({
+            opacity: 1
+        }, 300);
+        
+        con2.animate({
+            opacity: 1
+        }, 300);
+        
+        $(".container1 img").last().clone().prependTo(".container1").end().end().remove();
+        $(".container2 img").last().clone().prependTo(".container2").end().end().remove();
     });
-    
+
+    $(".right-arrow").click(function() {
+        con1.css("opacity", "0");
+        con2.css("opacity", "0");
+
+        con1.animate({
+            opacity: 1
+        }, 300);
+        
+        con2.animate({
+            opacity: 1
+        }, 300);
+
+        $(".container1 img").first().clone().appendTo(".container1").end().end().remove();
+        $(".container2 img").first().clone().appendTo(".container2").end().end().remove();
+    });
 });
